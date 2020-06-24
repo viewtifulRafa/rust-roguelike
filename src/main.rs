a256e350ed5bf73e691517726436c97c902d3b25
Rafael Pena Bello <rafael.pena.bello@gmail.com>
2020-06-24 13:38:49 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,83 @@
+use rltk::{Rltk, GameState,RGB, VirtualKeyCode};
+use specs::prelude::*;
+use std::cmp::{max,min};
+use specs_derive::Component;
+
+// Components 
+
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB,
+}
+
+#[derive(Component)]
+struct LeftMover {    
+}
+
+// System
+struct LeftWalker {}
+
+impl<'a> System<'a> for LeftWalker{
+    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+}
+
+struct State {
+    ecs: World
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos,render) in (&positions, &renderables).join(){
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build()?;
+    let mut gs = State{
+        ecs: World::new()
+    };
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+
+    gs.ecs
+        .create_entity()
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .build();
+        
+    for i in 0..10 {
+        gs.ecs
+            .create_entity()
+            .with(Position {x: i * 7, y: 20})
+            .with(Renderable {
+                glyph: rltk::to_cp437('☺'),
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .with(LeftMover{})
+            .build();
+    }
+
+    rltk::main_loop(context, gs)
+}
